6298b75803fa6cdbe061ec583186e2b07b60f400
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-04-01 14:26:28 +0530

    token work completed

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 import { User } from "../models/user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
 export const getAllUsers = async (req, res) => {
     const users = await User.find({});
     const keyword = req.query.keyword;
@@ -23,10 +25,15 @@ export const register = async (req, res) => {
   });
 
   const hashedPassword = await bcrypt.hash(password, 10);
+
+  user = await User.create({ name, email, password: hashedPassword });
+
+  const token = jwt.sign({ _id:user._id },process.env.JWT_SECRET);
   
-  await User.create({ name, email, password: hashedPassword });
-  
-  res.status(201).json({
+  res.status(201).cookie("token",token,{
+    httpOnly:true,
+    maxAge: 15 * 60 * 1000
+  }).json({
     success: true,
     message: "Registerd Successfully"
   })
