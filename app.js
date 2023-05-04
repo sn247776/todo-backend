bf9d1dfa13ee1cffab48d9e1dc9c24e779a1197d
Sagar Nirwal <62455271+sn247776@users.noreply.github.com>
2023-05-04 15:23:57 +0530

    cors for every site

--- app.js
+++ app.js
@@ -15,13 +15,15 @@ config({
 // Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+
+app.use(cors());
 
 // Using routes
 app.use("/api/v1/users", userRouter);
